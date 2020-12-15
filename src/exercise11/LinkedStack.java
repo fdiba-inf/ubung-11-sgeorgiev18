e0ad77a5e71a1acc2474df1635a6595c26bb18ec
sgeorgiev18 <simeon.georgiev@fdiba.tu-sofia.bg>
2020-12-15 15:53:02 +0000

    was fun

--- src/exercise11/LinkedStack.java
+++ src/exercise11/LinkedStack.java
@@ -5,16 +5,31 @@ public class LinkedStack {
 
     public boolean isEmpty() {
         // Check if stack has no elements
-        return false;
+
+        return topNode == null;
     }
 
     public void push(String data) {
         // Push element to stack
+        Node nextNode = new Node(data);
+        if (isEmpty()) {
+          topNode = nextNode;
+        } else {
+          nextNode.setNextNode(topNode);
+          topNode = nextNode;
+        }
     }
 
     public String pop() {
         // Pop element from stack
-        return null;
+        if (isEmpty()) {
+          return null;
+        } else {
+          String data = topNode.getData();
+          topNode = topNode.getNextNode();
+          return data;
+        }
+        
     }
 
     @Override
